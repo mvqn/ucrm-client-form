e3186ac4d7c8f2b4e75969c39c35821ba4eaa5e3
Ryan Spaeth <rspaeth@mvqn.net>
2018-10-15 23:25:41 -0700

    Namespaced slim RESTClient, likely to bring in full client shortly.

--- www/submit.php
+++ www/submit.php
@@ -1,14 +1,15 @@
 <?php
 
-// Require necessary libraries...
-require_once __DIR__ . "/src/RestClient.php";
+//require_once __DIR__."/src/RestClient.php";
+require_once __DIR__."/vendor/autoload.php";
+
+use MVQN\REST\RestClient;
 
 // Load the configuration file...
 //$config = include(__DIR__."/config.php");
 require_once __DIR__."/bootstrap.php";
 
-//Notify the user of a successful submission!
-echo "Thank you for your submission, a sales representative will contact you soon!";
+
 
 
 
@@ -87,7 +88,7 @@ $clientId = $inserted["id"];
 // Create the Contact data...
 $contact = [
     "email" => $_POST["email"],
-    "phone" => $_POST["phone"],
+    "phone" => $_POST["phone"] ?: "",
     "name" => $_POST["firstName"]." ".$_POST["lastName"],
     "isBilling" => true,
     "isContact" => true,
@@ -103,4 +104,90 @@ $contact = array_filter($contact, function($value)
 $inserted = $rest->post("/clients/$clientId/contacts", $contact);
 
 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$siteUrl = $config["site"]["url"];
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = $config["smtp"]["server"];              // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = $config["smtp"]["username"];        // SMTP username
+    $mail->Password = $config["smtp"]["password"];        // SMTP password
+    //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    //$mail->Port = 587;                                    // TCP port to connect to
+    $mail->Port = 25;
+
+    //Recipients
+    $mail->setFrom($config["smtp"]["sender"]["email"], $config["smtp"]["sender"]["name"]);
+    //$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+
+    foreach($config["smtp"]["recipients"] as $email => $name)
+    {
+        $mail->addAddress($email, $name);     // Add a recipient
+    }
+
+    $mail->addReplyTo($config["smtp"]["sender"]["email"], $config["smtp"]["sender"]["name"]);
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $config["smtp"]["subject"];
+
+    $link = $config["ucrm"]["host"]."/client/".$clientId;
+    $name = $contact['name'];
+    $email = $contact['email'];
+    $phone = (array_key_exists("phone", $contact) && $contact["phone"] !== null && $contact["phone"] !== "") ? "{$contact['phone']}" : "[NONE]";
+
+    $mail->Body    =
+        "Sales Team,<br/>".
+        "<br/>".
+        "A new client lead has been generated from an online form.<br/>".
+        "<br/>".
+        "<a href='$link'><strong>$name</strong></a><br/>".
+        "Email: <a href='mailto:$email'>$email</a><br/>".
+        "Phone: $phone";
+
+    $mail->AltBody =
+        "Sales Team,\r\n".
+        "\r\n".
+        "A new client lead has been generated from an online form.\r\n".
+        "\r\n".
+        "$name ( $link )\r\n".
+        "Email: $email\r\n".
+        "Phone: $phone";
+
+    $mail->send();
+
+    //Notify the user of a successful submission!
+    echo "Thank you for your submission, a sales representative will contact you soon!<br/><br/>";
+    echo "Click <a href='$siteUrl'>here</a> to return to our website.";
+    //echo "Redirecting...";
+    //sleep(3);
+    //header("Location: ".$_SERVER["HTTP_REFERER"]);
+
+}
+catch (Exception $e)
+{
+    //Notify the user of an unsuccessful submission!
+    echo "Message could not be sent. Mailer Error: ", $mail->ErrorInfo;
+
+    echo "We are sorry for the inconvenience, but please feel free to contact us by phone!<br/><br/>";
+    echo "Click <a href='$siteUrl'>here</a> to return to our website.";
+    //sleep(3);
+    //header("Location: ".$_SERVER["HTTP_REFERER"]);
+    //die();
+}
 
